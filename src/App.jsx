f0449787e08a39a0d3d2d2aa438097e13742ce6f
First-project <mehedihasan0937580@gmail.com>
2025-01-21 23:38:43 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Test from "./Test";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-4xl font-bold">
+          welcome to my portfolio
+        </h1>
+        <Test />
+      </div>
+    </>
+  );
+}
+
+export default App;
