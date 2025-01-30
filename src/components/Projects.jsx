aa77dcc49163871f7d1455380bb1fce5ca4ae724
First-project <mehedihasan0937580@gmail.com>
2025-01-30 17:43:47 +0600

    projectCard design done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,8 +7,6 @@ const Projects = () => {
       <Header>Projects</Header>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-start gap-5 mt-8">
         <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
       </div>
     </div>
   );
--- src/components/common/ProjectCard.jsx
+++ src/components/common/ProjectCard.jsx
@@ -1,26 +1,43 @@
-const ProjectCard = () => {
+import { FaGithub } from "react-icons/fa";
+import { VscOpenPreview } from "react-icons/vsc";
+const ProjectCard = ({ img, name, previewLink, codeLink, useCase }) => {
   return (
-    <div className="bg-activeColor/35 px-4 py-6 rounded-lg hover:scale-105 transition-all duration-300 ease-in-out">
+    <div className="bg-activeColor/35 px-4 py-6 rounded-lg hover:scale-105 transition-all duration-300 ease-in-out shadow-md shadow-sky-300 dark:shadow-black group">
       <img
-        src="/flower-bank.png"
-        alt="flower-bank project"
-        className="rounded-md shadow-md shadow-sky-400 h-[180px] w-[320px]"
+        src={img}
+        alt={name}
+        className="rounded-md shadow-md shadow-sky-400 h-[180px] w-[320px] hover:scale-x-90 duration-300 ease-in-out transition-all"
       />
-      <div className="mt-2">
-        <h3 className="font-inter font-semibold text-2xl sm:text-3xl text-center text-black">
-          Flower-bank
+      <div className="mt-3">
+        <h3 className="font-inter font-semibold text-2xl sm:text-3xl text-center text-slate-700 dark:text-sky-300">
+          {name}
         </h3>
-        <div className="mt-4">
+        <div className="mt-4 space-y-3">
           <p className="font-itim text-xl sm:text-2xl text-black">
             Front-end project
           </p>
-          <div className="flex gap-4">
-            <a href="" className="font-itim text-sm text-black/95 sm:text-base">
-              Preview
+          <div className="hidden group-hover:flex gap-4">
+            <a
+              href={previewLink}
+              className="flex items-center gap-1 font-inter font-semibold text-sm text-black/95 sm:text-base shadow-sm shadow-black px-2 py-0 rounded-md group-hover:bg-sky-300 group-hover:transition-all group-hover:duration-300 group-hover:text-red-500 hover:shadow-inner hover:shadow-black/50 "
+            >
+              <span>Preview</span>
+              <span>
+                <VscOpenPreview />
+              </span>
             </a>
-            <a href="" className="font-itim text-sm text-black/95 sm:text-base">
-              Code
+            <a
+              href={codeLink}
+              className="flex items-center gap-1 font-inter font-semibold text-sm text-black/95 sm:text-base shadow-sm shadow-black px-2 py-0 rounded-md group-hover:bg-sky-300 group-hover:transition-all group-hover:duration-300 group-hover:text-red-500 hover:shadow-inner hover:shadow-black/50 "
+            >
+              <span>Code</span>
+              <span>
+                <FaGithub />
+              </span>
             </a>
+          </div>
+          <div className="text-slate-900 ">
+            <p>{useCase}</p>
           </div>
         </div>
       </div>
