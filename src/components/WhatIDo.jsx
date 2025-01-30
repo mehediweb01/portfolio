e59814042e20f4414a5bf2ffe60302e4ee64125b
First-project <mehedihasan0937580@gmail.com>
2025-01-30 19:12:50 +0600

    what i do section update

--- src/components/WhatIDo.jsx
+++ src/components/WhatIDo.jsx
@@ -1,9 +1,8 @@
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaShapes } from "react-icons/fa6";
 import { FaLaptopCode } from "react-icons/fa";
-import { FaArrowRight } from "react-icons/fa";
-import { Buttons } from "./common/Button";
 import { Header } from "./common/Header";
+import { FaCode } from "react-icons/fa";
 const WhatIDo = () => {
   return (
     <div className="relative py-8">
@@ -28,22 +27,28 @@ const WhatIDo = () => {
             Front-end web developer
           </h1>
           <p className="font-itim text-base sm:text-xl leading-5 sm:leading-10 text-black max-w-lg dark:text-white">
-            Creative Front-End Developer | Building User-Friendly Web
-            Experiences
+            I create responsive, user-friendly, and modern web interfaces with
+            clean, efficient front-end code
           </p>
-          <Buttons
-            variant="solid"
-            className="bg-transparent font-itim text-xl sm:text-3xl leading-8 sm:leading-[50px] !text-black flex gap-2 items-center justify-center dark:!text-white"
-          >
-            contact <FaArrowRight className="text-2xl text-sky-400" />
-          </Buttons>
+        </div>
+        {/* 2nd card */}
+        <div className="px-6 py-8 rounded-lg shadow-md hover:shadow-black hover:shadow-inner shadow-black dark:shadow-inner dark:shadow-black space-y-4 border dark:border-none border-slate-100 dark:hover:shadow-black dark:hover:shadow-md">
+          <FaCode className="size-12 text-header" />
+          <h1 className="font-itim text-2xl sm:text-4xl leading-8 sm:leading-[48px] text-darkBlack dark:text-white">
+            Clean Code
+          </h1>
+          <p className="font-itim text-base sm:text-xl leading-5 sm:leading-10 text-black max-w-lg dark:text-white">
+            I strive to write clean, efficient, and scalable React code by
+            following key principles: Component Reusability, Optimized
+            Performance, Code Simplicity
+          </p>
         </div>
       </div>
       {/* setting animation */}
-      <div className="absolute right-4 md:right-[10%] top-[25%] shadow-inner shadow-sky-600 py-2 px-6 rounded-md">
+      <div className="absolute right-4 md:right-[10%] top-[20%] shadow-inner shadow-sky-600 py-2 px-6 rounded-md">
         <IoSettingsOutline className="size-5 animate-spin" />
       </div>
-      <div className="absolute left-4 md:left-[10%] top-[25%] shadow-inner shadow-sky-600 py-2 px-6 rounded-md">
+      <div className="absolute left-4 md:left-[10%] top-[20%] shadow-inner shadow-sky-600 py-2 px-6 rounded-md">
         <FaShapes className="size-5 animate-spin" />
       </div>
     </div>
