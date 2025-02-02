f73aa3d1eb19ec755622e85aff1de442db78e784
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2025-02-02 21:53:56 +0600

    faqs animation done

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -1,13 +1,19 @@
 import { FAQsInfo } from "../db";
 import FaqsCard from "./common/FaqsCard";
 import { Header } from "./common/Header";
+import { motion } from "motion/react";
 const FAQs = () => {
   return (
     <div id="faqs" className="py-16">
       <div>
         <Header>FAQs</Header>
       </div>
-      <div className="space-y-3 mt-16">
+      <motion.div
+        initial={{ opacity: 0, translateY: 35 }}
+        whileInView={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 1, ease: [0.43, 0.13, 0.28, 0.96] }}
+        className="space-y-3 mt-16"
+      >
         {FAQsInfo.map((item, index) => (
           <div key={index} className="w-[90%] mx-auto">
             <FaqsCard
@@ -17,7 +23,7 @@ const FAQs = () => {
             />
           </div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
