a9f71230a0ee1c166022157dfed710028a1203ef
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2025-02-16 19:56:36 +0600

    on load effect remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { RingLoader } from "react-spinners";
 import ScrollSpy from "react-ui-scrollspy";
 import {
   MyNavbar,
@@ -15,7 +14,7 @@ import {
 
 function App() {
   const [theme, setTheme] = useState("light");
-  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
@@ -23,38 +22,26 @@ function App() {
       document.documentElement.classList.remove("dark");
     }
   });
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
   const darkMode = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
     <>
-      {loading ? (
-        <div className="flex items-center justify-center h-screen bg-black/95">
-          <RingLoader color="#3499ee" size={130} />
-        </div>
-      ) : (
-        <div className="bg-white dark:bg-[#2B2C2F]">
-          <BrowserRouter>
-            <MyNavbar switchDarkMode={darkMode} isDarkMode={theme} />
-            <ScrollSpy offsetTop={-50} scrollThrottle={10}>
-              <About />
-              <WhatIDo />
-              <Skills />
-              <Projects />
-              <Contact />
-              <FAQs />
-            </ScrollSpy>
-            <Footer />
-          </BrowserRouter>
-        </div>
-      )}
+      <div className="bg-white dark:bg-[#2B2C2F]">
+        <BrowserRouter>
+          <MyNavbar switchDarkMode={darkMode} isDarkMode={theme} />
+          <ScrollSpy offsetTop={-50} scrollThrottle={10}>
+            <About />
+            <WhatIDo />
+            <Skills />
+            <Projects />
+            <Contact />
+            <FAQs />
+          </ScrollSpy>
+          <Footer />
+        </BrowserRouter>
+      </div>
     </>
   );
 }
